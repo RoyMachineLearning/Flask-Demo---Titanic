2a0b858b934cc27b36603d136c6ac5f3582c3356
Ashish Gupta <35936805+RoyMachineLearning@users.noreply.github.com>
2024-05-12 16:48:44 -0400

    Add files via upload

--- app titanic_html.py
+++ app titanic_html.py
@@ -4,7 +4,6 @@ import pandas as pd
 from flask import Flask, request, render_template
 import joblib
 import sys
-import numpy as np
 
 app = Flask(__name__)
 
@@ -17,23 +16,16 @@ def result():
     n_features = []
     for i in request.form.values():
         n_features.append(i)
-    cols = ['age', 'sex', 'pclass','embarked']
+    cols = ['age', 'sex', 'embarked']
     print(n_features)
     features = pd.DataFrame([n_features],columns=cols)
     print(features)
-    features['pclass'] = features['pclass'].astype(int)
     trans_data= pipeline.transform(features)
     print(trans_data)
-    predict_proba = lr.predict_proba(trans_data)
-    prediction = lr.predict(trans_data)
-    if prediction == 1:
-        Pred = "High"
-    else:
-        Pred = "Low"
-    
-    predict_proba = ",".join(map(str, np.round(predict_proba[:, 1]*100,2)))
-    predict_proba = predict_proba + "%"
-    return render_template("result.html", prediction = Pred, predict_proba = predict_proba)
+    scaled_df = scalar.transform(trans_data)
+    prediction = lr.predict(scaled_df)
+    print(prediction)
+    return render_template("result.html", prediction = prediction[0] )
 
 if __name__ == '__main__':
     try:
@@ -41,9 +33,12 @@ if __name__ == '__main__':
     except:
         port = 12345 # If you don't provide any port the port will be set to 12345
 
-    lr = joblib.load(r'C:\Users\ashish.gupta\Downloads\Flask Activity Titanic Project\my_model_titanic.pkl') # Load "model.pkl"
+    lr = joblib.load('C:\\Users\\GupAshis\Downloads\\Titanic_project\\Titanic_project\\my_model_titanic.pkl') # Load "model.pkl"
     print ('Model loaded')
-    pipeline = joblib.load(r'C:\Users\ashish.gupta\Downloads\Flask Activity Titanic Project\my_pipe_titanic.pkl')
+    pipeline = joblib.load('C:\\Users\\GupAshis\Downloads\\Titanic_project\\Titanic_project\\my_pipe_titanic.pkl')
     print ('Pipeline loaded')
-  
+    scalar = joblib.load('C:\\Users\\GupAshis\Downloads\\Titanic_project\\Titanic_project\\my_scalar_titanic.pkl')
+    print ('scalar loaded')
+    
+    
     app.run(port=port, debug=True)
--- titanic_model.py
+++ titanic_model.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 pd.set_option('display.max_columns',30) # set the maximum width
 # Load the dataset in a dataframe object 
-df = pd.read_csv(r'C:\Users\ashish.gupta\Downloads\Flask Activity Titanic Project\titanic3.csv')
+df = pd.read_csv('C:/courses_centennial/COMP309 new/data/titanic3.csv')
 # Explore the data check the column values
 print(df.columns.values)
 print (df.head())
@@ -19,10 +19,10 @@ print(df.isnull().sum())
 #or
 print(len(df) - df.count())
 # Check the strongest relationship with class from numeric fields
+print(df.corr())
 #Choose columns
-include = ['age','sex', 'embarked', 'survived','pclass']
+include = ['age','sex', 'embarked', 'survived']
 df_ = df[include]
-#print(df_.corr())
 """
 Explore again
 """
@@ -34,7 +34,6 @@ print(len(df_) - df.count())
 print(df_['sex'].unique())
 print(df_['embarked'].unique())
 print(df_['survived'].unique())
-print(df_['pclass'].unique())
 print(df_['survived'].value_counts())   # unbalanced
 """
 Split
@@ -64,43 +63,32 @@ scale the data
 """
 # separate target from features
 dependent_variable = 'survived'
-cols = ['age', 'sex', 'pclass','embarked']
-
 y_train = train_set[dependent_variable]
-x_train = train_set[cols]
+x_train = train_set[train_set.columns.difference([dependent_variable])]
 
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
-
+from sklearn.preprocessing import OneHotEncoder
 
 # Get column names
 cat_attribs = ['sex','embarked']
 num_attribs = ['age']
-ord_attribs = ['pclass']
-
-
-
+model_columns = list(train_set.columns)
 cat_pipeline = Pipeline(steps=[('mode_cat',SimpleImputer(strategy="most_frequent")),
-                           ("one_hot_encode",OneHotEncoder(drop='first'))])
+                           ("one_hot_encode",OneHotEncoder())])
+col_transform = ColumnTransformer(transformers=[("cat",cat_pipeline,[1,2]),
+                                               ("age",SimpleImputer(strategy="median"),[0])],
+                                               
+                                  )
 
-ord_pipeline = Pipeline(steps=[('mode_ord',SimpleImputer(strategy="most_frequent")),
-                           ("ordinal_encode",OrdinalEncoder())])
+x_train_prepared1=col_transform.fit_transform(x_train)
 
-num_pipeline = Pipeline(steps=[('mode_num',SimpleImputer(strategy="median")),
-                           ("num_encode",StandardScaler())])
+# still data is not scaled
 
-col_transform = ColumnTransformer(transformers=[("cat",cat_pipeline,cat_attribs),("pclass", ord_pipeline,ord_attribs),("age",num_pipeline,num_attribs)])
-
-#x_train.to_csv('test1.csv',index=False)
-x_train_prepared=col_transform.fit_transform(x_train)
-
-cols = ['sex', 'embarked_s', 'embarked_q', 'pclass','age']
-x_train_prepared = pd.DataFrame(x_train_prepared,columns=cols)
-
-#x_train_prepared.to_csv('test.csv',index=False)
+scalar = StandardScaler()
+x_train_scaled = scalar.fit_transform(x_train_prepared1)
 """
 Assume we are doing Logistic regression
 Let us build one with cross validation
@@ -113,7 +101,7 @@ lr = LogisticRegression(solver='lbfgs')
 from sklearn.model_selection import KFold
 crossvalidation = KFold(n_splits=10, shuffle=True, random_state=1)
 from sklearn.model_selection import cross_val_score
-score = np.mean(cross_val_score(lr,x_train_prepared, y_train, scoring='accuracy', cv=crossvalidation, n_jobs=1))
+score = np.mean(cross_val_score(lr,x_train_scaled, y_train, scoring='accuracy', cv=crossvalidation, n_jobs=1))
 print ('The score of the 10 fold run is: ',score)
 """
 Hyperparameter tune
@@ -126,7 +114,7 @@ pram_grid = dict(C=C, penalty=penalty)
 
 clf = RandomizedSearchCV(lr, pram_grid, random_state=42, n_iter=100,
                              cv=3, verbose=0, n_jobs=-1)
-clf.fit(x_train_prepared, y_train)
+clf.fit(x_train_scaled, y_train)
 
 print(clf.best_params_)
 best_model = clf.best_estimator_ 
@@ -137,9 +125,10 @@ Dump the model, pilepline & scalar
 """
 
 import joblib 
-joblib.dump(best_model, r'C:\Users\ashish.gupta\Downloads\Flask Activity Titanic Project\my_model_titanic.pkl')
+joblib.dump(best_model, 'C:/courses_centennial/COMP 247/Titanic_project/my_model_titanic.pkl')
 print("Model dumped!")
-joblib.dump(col_transform,r'C:\Users\ashish.gupta\Downloads\Flask Activity Titanic Project\my_pipe_titanic.pkl')
+joblib.dump(col_transform,'C:/courses_centennial/COMP 247/Titanic_project/my_pipe_titanic.pkl')
 print("Pipe dumped!")
-
+joblib.dump(scalar, 'C:/courses_centennial/COMP 247/Titanic_project/my_scalar_titanic.pkl')
+print("Scalar dumped!")
 
